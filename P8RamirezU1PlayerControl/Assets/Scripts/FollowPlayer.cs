b21e388215a63c500a223c9d6bbedeafbac558fd
Ry4nr <756238@student.dusd.net>
2025-03-10 15:27:12 -0700

    I added one bonus
    I made a bonus on adding a first person POV.

--- P8RamirezU1PlayerControl/Assets/Scripts/FollowPlayer.cs
+++ P8RamirezU1PlayerControl/Assets/Scripts/FollowPlayer.cs
@@ -6,6 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 backwards = new Vector3(0, 4, 1);
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +18,10 @@ public class FollowPlayer : MonoBehaviour
     {
         // Offset the camera behhind the player by adding to the players position
         transform.position = player.transform.position + offset ;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position = player.transform.position + backwards ;
+            transform.rotation = player.transform.rotation;
+        }
     }
 }
